3233b057853af4541dad55321e591aaf45193019
oreyes <Oreyes0806@gmail.com>
2024-06-06 13:55:30 -0500

    Actualizacion de footer

--- Frontend/src/componentes/Footer/Footer.jsx
+++ Frontend/src/componentes/Footer/Footer.jsx
@@ -3,7 +3,8 @@ export const Footer = () => {
   return (
     <footer className='footer'>
          <h2>Hostal Delicias</h2>
-         <p>Desarrollado por : Orlando</p>
+         <p>Desarrollado por : Orlando, Samuel
+         </p>
          <a href="https://github.com/Oreyes0806" target="_blank">
          <img width="42" height="42" src="https://img.icons8.com/ios-glyphs/30/github.png" alt="github"/>
          </a>
